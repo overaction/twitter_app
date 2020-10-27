686d49440d5247a97893e1c7354921292fd11312
overaction <52850392+overaction@users.noreply.github.com>
2020-10-27 17:51:12 +0900

    1-1. Firebase Api key setting

--- twitter/src/firebase.js
+++ twitter/src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyApNZk0szV5LQSN50Kfnww2r7ZY0UH9068',
-    authDomain: 'twitter-clone-a9244.firebaseapp.com',
-    databaseURL: 'https://twitter-clone-a9244.firebaseio.com',
-    projectId: 'twitter-clone-a9244',
-    storageBucket: 'twitter-clone-a9244.appspot.com',
-    messagingSenderId: '827746365789',
-    appId: '1:827746365789:web:5d90ec10e732c1300538b4',
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
