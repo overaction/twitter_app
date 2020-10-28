a9aa31045982468b109c13d5341f3ade86c20f8c
overaction <52850392+overaction@users.noreply.github.com>
2020-10-29 00:25:40 +0900

    10. Get Tweets

--- twitter/src/routes/Home.js
+++ twitter/src/routes/Home.js
@@ -1,11 +1,28 @@
 import { dbService } from 'myBase';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home =  () => {
   const [tweet, setTweet] = useState('');
+  const [tweets, setTweets] = useState([]);
+
+  const getTweets = async () => {
+    const getTweets =  await dbService.collection("tweets").get();
+    getTweets.forEach((doc) => {
+      const tweetObject = {
+        ...doc.data(),
+        id: doc.id,
+      }
+      setTweets((prev) => [tweetObject, ...prev])
+    })
+  }
+  useEffect(() => {
+    getTweets();
+  }, [])
+
+
   const onSubmit = async (e) => {
     e.preventDefault();
-    await dbService.collection("tweets").set({
+    await dbService.collection("tweets").add({
       tweet: tweet,
       createdAt: Date.now(),
     });
@@ -15,12 +32,20 @@ const Home =  () => {
     const {target: {value}} = e;
     setTweet(value);
   }
+  console.log(tweets);
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input value={tweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
         <input type="submit" value="Twitt" />
       </form>
+      <div>
+        {tweets.map(tweet => (
+          <div key={tweet.id}>
+            <h4>{tweet.tweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
