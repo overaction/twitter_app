db9bc37b21a76059349a5a614ada106fde3c7355
overaction <52850392+overaction@users.noreply.github.com>
2020-10-28 22:38:54 +0900

    8. Home and Database setup

--- twitter/src/routes/Home.js
+++ twitter/src/routes/Home.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Home =  () => <span>Home</span>;
+const Home =  () => {
+  const [tweet, setTweet] = useState('');
+  const onSubmit = (e) => {
+    e.preventDefault();
+  }
+  const onChange = (e) => {
+    const {target: {value}} = e;
+    setTweet(value);
+  }
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={tweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+        <input type="submit" value="Twitt" />
+      </form>
+    </div>
+  )
+}
 
 export default Home;
