b1948556da052f8d60f67e696ec943120e14c2b8
overaction <52850392+overaction@users.noreply.github.com>
2020-10-29 22:43:21 +0900

    13. Delete and Editing Button - 2

--- twitter/src/components/Tweet.js
+++ twitter/src/components/Tweet.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import { dbService } from 'myBase';
+import React, { useState } from 'react';
 
 const Tweet = ({ tweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newTweet, setNewTweet] = useState(tweetObj.text);
+
+  const onDeleteClick = async () => {
+    const ok = window.confirm('Are you sure you want to delete this tweet?');
+    if (ok) {
+      await dbService.doc(`tweets/${tweetObj.id}`).delete();
+    }
+  };
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await dbService.doc(`tweets/${tweetObj.id}`).update({text: newTweet});
+    setEditing(false);
+  };
+  const onTweetChange = (e) => {
+    setNewTweet(e.target.value);
+  };
   return (
     <div>
-      <h4>{tweetObj.text}</h4>
-      {isOwner && (
+      {editing ? (
         <>
-          <button>Delete Tweet</button>
-          <button>Edit Tweet</button>
+          <form onSubmit={onSubmit}>
+            <input
+              onChange={onTweetChange}
+              type="text"
+              placeholder="what is your tweet"
+              value={newTweet}
+              required
+            />
+            <input type="submit" value="Update Tweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{tweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Tweet</button>
+              <button onClick={toggleEditing}>Edit Tweet</button>
+            </>
+          )}
         </>
       )}
     </div>
